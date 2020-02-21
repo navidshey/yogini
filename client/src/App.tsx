5fbae29ab67881ef8ac885eb5aaaebcd41ec1153
navid <sheydaein@gmail.com>
2020-02-21 16:22:27 +0330

    start using  redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 import NavBar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
 import "./App.css";
+
 
 function App() {
   return (
+    <Provider store={ store }>
     <Router>
       <div className="App">
         <NavBar />
@@ -20,6 +27,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/reducers/authReducer.ts
@@ -0,0 +1,11 @@
+const initialState ={
+    isAuthenticatd: false,
+    user: {}
+}
+
+export default function(state=initialState, action){
+    switch (action.type) {
+        default:
+            return stete;
+    }
+}
